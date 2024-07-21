ffaca775eaa6eb98e52fce1086a113db0949d9ca
none <none>
2024-07-21 13:40:36 +0200

    Add colorSchemeOf() util

--- lib/screens/aboutus_screen.dart
+++ lib/screens/aboutus_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flut_crack/utils/theme_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flut_crack/widgets/nav.dart';
 
@@ -6,11 +7,14 @@ class AboutUsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    final colorScheme = colorSchemeOf(context);
+
     return Scaffold(
       drawer: const NavDrawer(),
       appBar: AppBar(
         title: const Text("About FlutCrack"),
-        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        backgroundColor: colorScheme.primaryContainer,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -20,20 +24,20 @@ class AboutUsScreen extends StatelessWidget {
             ListTile(
               leading: Icon(
                 Icons.thumb_up, 
-                color: Theme.of(context).colorScheme.primary
+                color: colorScheme.primary
               ),
               title: const Text("FlutCrack 1.1"),
             ),
             ListTile(
-              leading: Icon(Icons.person, color: Theme.of(context).colorScheme.primary),
+              leading: Icon(Icons.person, color: colorScheme.primary),
               title: const Text("Creator: Giovanni Iannaccone"),
             ),
             ListTile(
-              leading: Icon(Icons.balance, color: Theme.of(context).colorScheme.primary),
+              leading: Icon(Icons.balance, color: colorScheme.primary),
               title: const Text("License: GPL-3.0"),
             ),
             ListTile(
-              leading: Icon(Icons.code, color: Theme.of(context).colorScheme.primary),
+              leading: Icon(Icons.code, color: colorScheme.primary),
               title: const Text("Source code:"),
             ),
             Padding(
@@ -41,7 +45,7 @@ class AboutUsScreen extends StatelessWidget {
               child: Text(
                 "https://github.com/giovanni-iannaccone/FlutCrack",
                 style: TextStyle(
-                  color: Theme.of(context).colorScheme.secondary,
+                  color: colorScheme.secondary,
                   decoration: TextDecoration.underline,
                 ),
               ),
---
+++ lib/utils/theme_utils.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+ColorScheme colorSchemeOf(BuildContext context){
+  return Theme.of(context).colorScheme;
+}
